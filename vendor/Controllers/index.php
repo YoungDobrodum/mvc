bc165ca112081f22d3b92e6326255d2824293ea6
YoungDobrodum <103575155+YoungDobrodum@users.noreply.github.com>
2022-04-21 18:37:39 +0300

    Update index.php

--- vendor/Controllers/index.php
+++ vendor/Controllers/index.php
@@ -7,4 +7,9 @@ class index
     public function index(){
         echo 'index controller index action';
     }
+    
+    public function store()
+    {
+        echo 'index controller store method';
+    }
 }
