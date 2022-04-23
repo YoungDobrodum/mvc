27fcadad6081ac2169510eac5e6881840634b31f
YoungDobrodum <yung.dobrodum@gmail.com>
2022-04-23 11:58:21 +0300

    views default template and index_index_page

---
+++ vendor/Views/index_index_page.php
@@ -0,0 +1,8 @@
+<h2>Home</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda autem, facilis iure nesciunt non qui sint
+    voluptatum! Ab aperiam deleniti iste omnis quos. Consectetur delectus doloremque dolores eaque eius enim et ex
+    excepturi exercitationem facilis fuga ipsam magni minima necessitatibus neque nisi non odit optio perferendis
+    praesentium quas qui quis quisquam, ratione recusandae sequi soluta sunt velit vitae voluptatibus? Consectetur, cum
+    dolorem error facilis illum inventore ipsam mollitia neque nostrum, obcaecati odit perferendis perspiciatis placeat
+    quaerat quos sapiente similique! Aspernatur blanditiis culpa cumque debitis fuga laboriosam necessitatibus nulla
+    soluta totam.</p>
