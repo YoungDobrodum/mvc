11f1470bfa811b410fa8fc4cf6d8737d2b3caf66
YoungDobrodum <yung.dobrodum@gmail.com>
2022-04-23 11:57:19 +0300

    View class

---
+++ vendor/Core/View.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Core;
+
+class View
+{
+    const VIEWS_DIR = 'Views';
+    /**
+     * name of template file
+     * @var string|
+     */
+    protected $template = 'default';
+
+    /**
+     *
+     * @param ?string $template
+     */
+    public function __construct(string $template = null)
+    {
+        if($template !== null){
+            $this->template = $template;
+        }
+    }
+
+    public function render(string $pageTemplate, array $data = []){
+        extract($data);
+        include_once 'vendor'.DIRECTORY_SEPARATOR.self::VIEWS_DIR.DIRECTORY_SEPARATOR.$this->template.'.php';
+    }
+}
