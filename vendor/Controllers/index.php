524f5f2f9fe35d31b7180a1e29cab00bdcdfba81
YoungDobrodum <yung.dobrodum@gmail.com>
2022-04-26 19:22:39 +0300

    controller Index and Note

--- vendor/Controllers/index.php
+++ vendor/Controllers/index.php
@@ -4,14 +4,11 @@ namespace Controllers;
 
 use Core\View;
 
-class index
+class index extends AbstractController
 {
-    protected $view;
-
-    public function __construct()
-    {
-        $this->view = new View();
-    }
+    /**
+     * @var View
+     */
 
     public function index(){
         $this->view->render('index_index_page');
