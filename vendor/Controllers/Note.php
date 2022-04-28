14c7315b2df29e031ec3b497e1ec9954b33c3b35
YoungDobrodum <yung.dobrodum@gmail.com>
2022-04-28 20:26:23 +0300

    controller add create and store actions

--- vendor/Controllers/Note.php
+++ vendor/Controllers/Note.php
@@ -7,12 +7,38 @@ use Models\NoteModel;
 
 class Note extends AbstractController
 {
+
+    protected $model;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->model = new NoteModel();
+    }
+
     public function index()
     {
-        $model = new NoteModel();
-        $notes = $model->all();
+
         $this->view->render('note_index_page',
-            ['notes'=>$model->all(),
+            ['notes' => $this->model->all(),
             ]);
+    }
+
+    public function create()
+    {
+        $this->view->render('note_create_page', ['errors' => \Route::getErrors()]);
+    }
+
+    public function store()
+    {
+        $noteText = filter_input(INPUT_POST, 'note');
+        $errors = [];
+        if(empty($noteText)){
+            $errors[] = 'Note can not be empty';
+            \Route::addErrors($errors);
+            \Route::redirect($_SERVER['HTTP_REFERER']);
+        }
+        $this->model->add($noteText);
+        \Route::redirect(\Route::url('note'));
     }
 }
